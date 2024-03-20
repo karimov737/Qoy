b1840a46410b497b7542e25aef2e831c35ebaef1
Bobirjon <karimovbobir24@gmail.com>
2024-03-20 12:31:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let box =""
+// if (a > 0 && a < 2) {box = a+ " qoy...";
+    
+// } else {
+//     box = a+ " qoylar...";
+// }
+// console.log(box)
+
+
+
+//  let a = +prompt("Son kriting:")
+// let box =""
+
+// for(let i=0;i<a;i++){
+// if (a > 0 && a < 2) {
+//     box = a+ " qoy...";
+    
+// } else {
+//     box = a+ " qoylar...";
+// }
+// console.log(box)
+
+// }
+// let a = +prompt("Son kriting:");
+// let box = "";
+
+// for (let i = 0; i < a; i++) {
+//     if (a > 0 && a < 2) {
+//         box = a + " qoy...";
+//     } else {
+//         box = a + " qoylar...";
+//     }
+//     console.log(box);
+// }
+let a = +prompt("Son kriting:");
+let box = "";
+for (let i = 1; i <= a; i++) {
+    if (i > 0 && i < 2) {
+        box = 1 + " qoy...";
+    } else {
+        box = i + " qoylar...";
+    }
+    console.log(box);
+}
